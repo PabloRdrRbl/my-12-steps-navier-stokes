71105eed85f71b751e0c171e9a1e02fcfc799697
pablordrrbl <prodrr05@estudiantes.unileon.es>
2016-07-06 18:51:18 +0200

    Add Step 1

---
+++ step1.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import sys
+
+# Evenly spaced grid (uniformemente distribuida)
+nx = 41
+dx = 2 / (nx - 1)  # Spatial domain has 2 units length
+nt = 25  # Number of time steps
+dt = 0.025  # Amount of time each time steps takes
+c = 1  # Wave speed
+
+# Initial conditions
+u = np.ones(nx)
+u[0.5 / dx:1 / dx + 1] = 2  # u = 2 between 0.5 and 1
+
+# Printing u
+plt.plot(np.linspace(0, 2, nx), u)
+plt.show()
+
+un = np.ones(nx)  # Initialize a temporary array
+
+for n in range(nt):
+    un = u.copy()
+    for i in range(1, nx):
+        u[i] = un[i] - c * dt / dx * (un[i] - un[i - 1])
+
+# Printing the new u
+plt.plot(np.linspace(0, 2, nx), u)
+plt.show()
