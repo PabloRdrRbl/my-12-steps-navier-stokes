6a2e8f884cde243874422ba52af64c4b0e8a981a
pablordrrbl <prodrr05@estudiantes.unileon.es>
2016-07-11 21:40:21 +0200

    Step 5 finished

--- Step-5/step5.py
+++ Step-5/step5.py
@@ -3,9 +3,6 @@ import matplotlib.pyplot as plt
 
 from mpl_toolkits.mplot3d import Axes3D
 
-
-plt.ion()  # Allows interactiva plotings
-
 # Grid parameters
 nx = 81
 ny = 81
@@ -20,16 +17,29 @@ x = np.linspace(0, 2, nx)
 y = np.linspace(0, 2, ny)
 
 u = np.ones((nx, ny))
-un = u.copy()
 
 # Initial conditions
 u[0.5 / dy: 1 / dy + 1, 0.5 / dx: 1 / dx + 1] = 2  # Hat function
 
 # Plot initial conditions
 fig = plt.figure(figsize=(11, 7), dpi=100)
-ax = fig.gca(projection='3d')
+ax = fig.add_subplot(111, projection='3d')
 X, Y = np.meshgrid(x, y)
-surf = ax.plot_surface(X, Y, u[:])
+ax.plot_wireframe(X, Y, u, color='blue')
 
-plt.draw()
-plt.pause(5)
+# Iterating in two dimensions
+for n in range(nt + 1):
+    un = u.copy()
+    row, col = u.shape
+    u[1:, 1:] = (un[1:, 1:] - (c * dt / dx * (un[1:, 1:] - un[1:, :-1])) -
+                 (c * dt / dy * (un[1:, 1:] - un[:-1, 1:])))
+    # Boundary conditions
+    u[0, :] = 1
+    u[-1, :] = 1
+    u[:, 0] = 1
+    u[:, -1] = 1
+
+
+ax.plot_wireframe(X, Y, u, color='blue')
+
+plt.show()
